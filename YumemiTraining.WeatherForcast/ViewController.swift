a86987176a71c819fe3d731e128d3fd2f27a59f2
Tomohiro Kumagai <tomohiro@ez-style.jp>
2021-08-05 16:39:23 +0900

    Initial Commit

---
+++ YumemiTraining.WeatherForcast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YumemiTraining.WeatherForcast
+//
+//  Created by Tomohiro Kumagai on 2021/08/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
