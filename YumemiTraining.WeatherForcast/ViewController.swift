d30b92719010a5a1421473dd016e134d293ac8f0
Tomohiro Kumagai <tomohiro@ez-style.jp>
2021-08-17 18:23:29 +0900

    Replace the API used for fetching a weather.

--- YumemiTraining.WeatherForcast/ViewController.swift
+++ YumemiTraining.WeatherForcast/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var weatherImageView: UIImageView!
     
+    /// The area that supposing we are in.
+    var currentArea = "tokyo"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,16 +28,54 @@ class ViewController: UIViewController {
 
 extension ViewController {
     
+    /// Present an error alert.
+    /// - Parameters:
+    ///   - message: The message to convey to a user.
+    ///   - ofTitle: the text for this alert.
+    ///   - actionHandler: The action that will be invoked
+    ///                    when touch the 'OK' button in the alert.
+    func presentErrorAlert(message: String, ofTitle: String, actionHandler: ((_ action: UIAlertAction) -> Void)? = nil) {
+    
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: actionHandler)
+        
+        alertController.addAction(okAction)
+        
+        present(alertController, animated: true)
+    }
+    
     /// Fetch current weather state and show in `weatherImageView`.
     func reloadWeather() {
         
-        let weatherString = YumemiWeather.fetchWeather()
-        
-        guard let weather = Weather(rawValue: weatherString) else {
+        do {
 
-            fatalError("Unexpected weather '\(weatherString)' was fetched.")
-        }
+            let weatherString = try YumemiWeather.fetchWeather(at: currentArea)
         
-        weatherImageView.image = weather.imageWithTintColor
+            guard let weather = Weather(rawValue: weatherString) else {
+                
+                fatalError("Unexpected weather '\(weatherString)' was fetched.")
+            }
+            
+            weatherImageView.image = weather.imageWithTintColor
+        }
+        catch let error as YumemiWeatherError {
+            
+            let title = "Failed to fetch weather"
+            let message: String
+            
+            switch error {
+            
+            case .invalidParameterError:
+                message = "The parameter '\(currentArea)' was not valid."
+            case .unknownError:
+                message = "A unknown error occurred while fetching weather in \(currentArea)."
+            }
+
+            presentErrorAlert(message: message, ofTitle: title)
+        }
+        catch {
+            
+            fatalError("Unexpected error: \(error)")
+        }
     }
 }
